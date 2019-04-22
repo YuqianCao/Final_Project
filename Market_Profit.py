faf16adaf01e24baa2e1f4fb40d33be9a8eaa791
“YuqianCao” <yuqianc1996@gmail.com>
2019-04-22 12:02:46 -0500

    update preconditions and set random seed

--- Market_Profit.py
+++ Market_Profit.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(5902019)
 
 price_level = {'level1': 5,
                'level2': 15,
@@ -56,7 +57,7 @@ class Company:
     def __init__(self,budget:int):
         self.budget = budget
     def getMaxamount(self,Market):
-        return int(self.budget/Market.getCost())
+        return self.budget/Market.getCost()
 
 
 def getMap(row:int, col:int, rich_area:list, poor_area:list):
@@ -165,7 +166,7 @@ def levelProfitPlot(map,resident,company,size):
     profit_uni = []
     for level in level_list:
         test_market = Market((0, 0), level, size)
-        loc = randomStoreLoc(map, company.getMaxamount(test_market), True)
+        loc = randomStoreLoc(map, int(company.getMaxamount(test_market)), True)
         markets_list = getMarkets(loc, level, size)
         profit_uni.append(getTotalProfit(markets_list, resident))
     plt.title('The Influence of Market Distribution')
@@ -173,7 +174,7 @@ def levelProfitPlot(map,resident,company,size):
     profit_rand = []
     for level in level_list:
         test_market = Market((0, 0), level, size)
-        loc = randomStoreLoc(map, company.getMaxamount(test_market), False)
+        loc = randomStoreLoc(map, int(company.getMaxamount(test_market)), False)
         markets_list = getMarkets(loc, level, size)
         profit_rand.append(getTotalProfit(markets_list, resident))
     plt.plot(level_list, profit_rand, 'r^-', label="Random Distribution")
@@ -181,13 +182,12 @@ def levelProfitPlot(map,resident,company,size):
     plt.ylabel('Total Profit')
     plt.legend()
     plt.savefig('MarkerDistributionVsProfit')
-    plt.show()
     plt.close('all')
 
 
 def main():
 
-    companyA = Company(10000)
+    companyA = Company(15000)
     city_row = 100
     city_col = 100
     cityMap = getMap(city_row, city_col, [[0, 0], [40, 40]], [[60, 60], [99, 99]])
@@ -209,8 +209,6 @@ def main():
     # simulation for hypothesis two:
     # simulation for random distribution and even distribution
     levelProfitPlot(cityMap,cityResident,companyA,40)
-
-
 
 
 
--- README.md
+++ README.md
@@ -9,6 +9,15 @@ Yuqian Cao (NetID: yuqianc3)
 ### Monte Carlo Simulation Scenario:
 There are many factors may influence the profit of a supermarket. For instance, the pricing, location, customer volume, hiring cost. Suppose there is a company that wants to start a supermarket chain in a city and they have already known the resident’s income level. This simulation could be used to help them decide the size, price level, location and how many supermarkets to open in this city to maximize the profit.
 
+### Preconditions: 
+1. In my simulation, the map of a city is in a rectangular shape. User can input the city's rows and columns to create a city map. 
+2. We assume that each element in the map array has a corresponding resident. The income level of each household is demonstrate by the value of the element.   
+    0: Low income level househol
+    1: Average income level household
+    2: High income level household
+3. For each resident, they have a shopping budget which is related to their income. And for each market, it has a average selling price. Residents are willing to shop in the market only if the selling price is lower or equal to their budget.
+4. For each resident, their maximum tolerance toward the distance to the market is determined by the size of the market. People can tolerate longer distance if the market is bigger.
+
 ### Hypotheses: 
 
 1. The best size and amount of supermarkets to be started is related to the population and income level.
